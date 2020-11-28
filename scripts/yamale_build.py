2de6d334c5670678c2b1c0270d0539bde20e2f64
Jordan Munch O'Hare <github@jordan.munchohare.com>
2020-11-28 08:50:14 +0100

    Integrity Check 3

--- scripts/yamale_build.py
+++ scripts/yamale_build.py
@@ -1,11 +1,11 @@
 import sys
 import os
-sys.path.append(os.path.abspath('../'))
+sys.path.append(os.path.abspath(''))
 
 from brain_brew.representation.build_config.top_level_builder import TopLevelBuilder
 
 build: str = TopLevelBuilder.build_yamale()
-filepath = "../brain_brew/schemas/recipe.yaml"
+filepath = "brain_brew/schemas/recipe.yaml"
 
 with open(filepath, 'w') as fp:
     fp.write(build)
