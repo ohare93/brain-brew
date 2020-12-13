74ec602482108e10e4838415d3b9cad87086abbe
Jordan Munch O'Hare <github@jordan.munchohare.com>
2020-12-13 18:12:22 +0100

    Media saving maintains subfolder status of existing files when 'recurring' is enabled

--- brain_brew/transformers/media_group_save_to_location.py
+++ brain_brew/transformers/media_group_save_to_location.py
@@ -9,19 +9,22 @@ from brain_brew.utils import create_path_if_not_exists
 def save_media_groups_to_location(
         parts: List[MediaGroup],
         folder: str,
-        clear_folder: bool
+        clear_folder: bool,
+        recursive: bool
 ) -> Set[MediaFile]:
 
-    existing_media_group = MediaGroup.from_directory(folder, False)  # TODO: Use recurring?
+    existing_media_group = MediaGroup.from_directory(folder, recursive)
     all_media_group = MediaGroup.from_many(parts)
 
     in_both, to_move, to_delete = all_media_group.compare(existing_media_group)
 
     create_path_if_not_exists(folder)
     for filename, media_file in all_media_group.media_files.items():
-        # if filename in in_both:
-        #    pass  # TODO: Check if copying is needed?
-        media_file.copy_self_to_target(folder)
+        if filename in in_both:
+            media_file.copy_self_to_target(existing_media_group.media_files[filename].file_path)
+            # TODO: Check if copying is needed?
+        elif filename in to_move:
+            media_file.copy_self_to_target(folder)
 
     if clear_folder and to_delete:
         deleted = '\n'.join(to_delete)
