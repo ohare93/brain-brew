da3089a0c0633fe86bc6a3430979944babf23a47
Jordan Munch O'Hare <github@jordan.munchohare.com>
2022-08-23 08:18:30 +0200

    More Header Overrides

--- brain_brew/build_tasks/overrides/headers_override.py
+++ brain_brew/build_tasks/overrides/headers_override.py
@@ -17,25 +17,39 @@ class HeadersOverride(YamlRepr):
     def yamale_schema(cls) -> str:
         return f'''\
             deck_description_html_file: str(required=False)
+            crowdanki_uuid: str(required=False)
+            name: str(required=False)
         '''
 
     @dataclass
     class Representation(RepresentationBase):
         deck_description_html_file: Optional[str]
+        crowdanki_uuid: Optional[str]
+        name: Optional[str]
 
     @classmethod
     def from_repr(cls, data: Union[Representation, dict]):
         rep: cls.Representation = data if isinstance(data, cls.Representation) else cls.Representation.from_dict(data)
         return cls(
             rep=rep,
-            deck_desc_html_file=HTMLFile.create_or_get(rep.deck_description_html_file)
+            deck_desc_html_file=HTMLFile.create_or_get(rep.deck_description_html_file),
+            crowdanki_uuid=rep.crowdanki_uuid,
+            name=rep.name
         )
 
     rep: Representation
     deck_desc_html_file: Optional[HTMLFile]
+    crowdanki_uuid: Optional[str]
+    name: Optional[str]
 
     def override(self, header: Headers):
         if self.deck_desc_html_file:
             header.description = self.deck_desc_html_file.get_data(deep_copy=True)
+
+        if self.crowdanki_uuid:
+            header.crowdanki_uuid = self.crowdanki_uuid
+
+        if self.name:
+            header.name = self.name
 
         return header
