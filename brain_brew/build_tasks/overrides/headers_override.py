9c54e3d59436e46a7e0146278955c2f90801aa90
Jordan Munch O'Hare <github@jordan.munchohare.com>
2022-08-23 13:39:11 +0200

    Fix for optional variables

--- brain_brew/build_tasks/overrides/headers_override.py
+++ brain_brew/build_tasks/overrides/headers_override.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Optional, Union
 
 from brain_brew.configuration.representation_base import RepresentationBase
@@ -16,30 +16,30 @@ class HeadersOverride(YamlRepr):
     @classmethod
     def yamale_schema(cls) -> str:
         return f'''\
-            deck_description_html_file: str(required=False)
             crowdanki_uuid: str(required=False)
+            deck_description_html_file: str(required=False)
             name: str(required=False)
         '''
 
     @dataclass
     class Representation(RepresentationBase):
-        deck_description_html_file: Optional[str]
-        crowdanki_uuid: Optional[str]
-        name: Optional[str]
+        crowdanki_uuid: Optional[str] = field(default=None)
+        deck_description_html_file: Optional[str] = field(default=None)
+        name: Optional[str] = field(default=None)
 
     @classmethod
     def from_repr(cls, data: Union[Representation, dict]):
         rep: cls.Representation = data if isinstance(data, cls.Representation) else cls.Representation.from_dict(data)
         return cls(
             rep=rep,
-            deck_desc_html_file=HTMLFile.create_or_get(rep.deck_description_html_file),
             crowdanki_uuid=rep.crowdanki_uuid,
+            deck_desc_html_file=HTMLFile.create_or_get(rep.deck_description_html_file),
             name=rep.name
         )
 
     rep: Representation
-    deck_desc_html_file: Optional[HTMLFile]
     crowdanki_uuid: Optional[str]
+    deck_desc_html_file: Optional[HTMLFile]
     name: Optional[str]
 
     def override(self, header: Headers):
